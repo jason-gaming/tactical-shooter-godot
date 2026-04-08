205d26f11f3c01a791fe92a5ed926dacadb546d7
jason-gaming <jasonronceray2@gmail.com>
2026-04-08 14:29:09 +0200

    Update game_constants.gd to be Godot 4 compatible with extends Node and const declarations

--- scripts/constants/game_constants.gd
+++ scripts/constants/game_constants.gd
@@ -1,55 +1,6 @@
-# Economy Constants
-BASE_CURRENCY_VALUE = 100
+extends Node
 
-WEAPON_PURCHASE_COST = {
-    'pistol': 250,
-    'rifle': 1500,
-    'sniper': 3000,
-}
-
-AGENT_DEPLOYMENT_COST = {
-    'soldier': 1000,
-    'sniper': 2500,
-    'medic': 1500,
-}
-
-# Weapons Constants
-MAX_AMMO = {
-    'pistol': 15,
-    'rifle': 30,
-    'sniper': 5,
-}
-
-RELOAD_TIME = {
-    'pistol': 1.5,
-    'rifle': 2.5,
-    'sniper': 3.0,
-}
-
-# Agents Constants
-AGENT_HEALTH = {
-    'soldier': 100,
-    'sniper': 75,
-    'medic': 80,
-}
-
-AGENT_SPEED = {
-    'soldier': 5.0,
-    'sniper': 4.0,
-    'medic': 4.5,
-}
-
-# Game Balance Constants
-WEAPON_DAMAGE = {
-    'pistol': 20,
-    'rifle': 35,
-    'sniper': 90,
-}
-
-AGENT_ARMOR = {
-    'soldier': 20,
-    'sniper': 10,
-    'medic': 5,
-}
-
-ROUND_TIME = 300
+const PLAYER_SPEED = 200
+const ENEMY_SPEED = 150
+const GRAVITY = 9.8
+const JUMP_HEIGHT = 30.0
